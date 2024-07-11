b1a96cc9167e0f929c76d93b13e87b4765e251f5
Rubylenshy <reztomoloju@gmail.com>
2024-07-11 17:07:21 +0100

    create react app react-class-demo-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [advice, setAdvice] = useState("");
+	const [count, setCount] = useState(0);
+
+	async function getAdvice() {
+		const res = await fetch('https://api.adviceslip.com/advice')
+		const data = await res.json();
+
+		setAdvice(data.slip.advice)
+		setCount( c => c + 1)
+	}
+
+	useEffect( function () {}, [])
+
+    return (
+        <div className="App">
+            <h1>{advice ? advice : 'hello World!'}</h1>
+            <button onClick={getAdvice}>Get Advice</button>
+			<Message count={count} />
+        </div>
+    );
+}
+
+function Message(props) {
+	return (
+		<p>
+			You have read <strong>{props.count}</strong> messages
+		</p>
+	)
 }
 
 export default App;
