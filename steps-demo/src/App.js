289ad330ee1ee516c43f6d57f867cae52bcb70d3
Rubylenshy <reztomoloju@gmail.com>
2024-07-18 23:59:12 +0100

    update

--- steps-demo/src/App.js
+++ steps-demo/src/App.js
@@ -1,35 +1,47 @@
 import { useState } from "react";
 import "./App.css";
 
+const messages = [
+	"Learn React",
+	"Apply for Jobs",
+	"Invest in your new income"
+];
+
 function App() {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 	const stepNumbers = [1,2,3];
 
 	function increaseStep() {
 		if (step < 3) {
-			setStep(step+1)
+			setStep((s) => s + 1)
 		}
 	}
 
 	function decreaseStep() {
 		if (step > 1) {
-			setStep(step-1)	
+			setStep((s) => s - 1)
 		}
 	}
 
     return (
-        <div className="steps">
-            <div className="numbers">
-				{ stepNumbers.map(num => <div className={step >= num ? 'active' : ''}>{num}</div>) }
-            </div>
+		<>
+			<button className="close" onClick={() => setIsOpen(() => !isOpen)}>&times;</button>
+			{isOpen && 
+				<div className="steps">
+					<div className="numbers">
+						{ stepNumbers.map(num => <div key={num} className={step >= num ? 'active' : ''}>{num}</div>) }
+					</div>
 
-            <p className="message">{step === 1 ? 'Learn React!': step === 2 ? 'Get a job' : 'Enjoy 👏'}</p>
+					<p className="message">Step {step}: {messages[step - 1]}</p>
 
-            <div className="buttons">
-                <button className="active" onClick={decreaseStep}>Previous</button>
-                <button className="active" onClick={increaseStep}>Next</button>
-            </div>
-        </div>
+					<div className="buttons">
+						<button className="active" onClick={decreaseStep}>Previous</button>
+						<button className="active" onClick={increaseStep}>Next</button>
+					</div>
+				</div>
+			}
+		</>
     );
 }
 
